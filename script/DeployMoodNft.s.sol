5133c1292a54df0529932343508dbd19b7b2b9c2
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-23 13:13:57 +0200

    update contracts✅

--- script/DeployMoodNft.s.sol
+++ script/DeployMoodNft.s.sol
@@ -7,14 +7,15 @@ import {MoodNft} from "../src/MoodNft.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract DeployMoodNft is Script {
-    function run() external returns (MoodNft) {
-    }
+    function run() external returns (MoodNft) {}
 
     function svgToImageURI(
         string memory svg
     ) public pure returns (string memory) {
         string memory baseURL = "data:image/svg+xml;base64,";
-        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
-    };
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svg)))
+        );
+        return string(abi.encodePacked(baseURL, svgBase64Encoded));
+    }
 }
-  }
