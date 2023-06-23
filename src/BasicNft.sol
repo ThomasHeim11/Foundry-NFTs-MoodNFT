aac03597e19e17249f004ea1bb442d2466f20ade
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-23 10:21:22 +0200

    update contracts✅

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -2,8 +2,19 @@
 
 pragma solidity ^0.8.18;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
 contract BasicNft is ERC721 {
+    uint256 private s_tokenCounter;
+
     constructor() ERC721("Dogie", "Dog") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft()  public {}
+
+    function tokenURI{uint256 toekId} public view override returns (stirng memory) {
+        return 
     }
 }
 
