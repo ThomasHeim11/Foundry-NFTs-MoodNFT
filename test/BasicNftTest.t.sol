3939ef49f20c6aae2c02cdbcce6386682d1eadc2
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-23 11:15:39 +0200

    update contracts✅

--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -5,10 +5,14 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
 import {Test, console} from "forge-std/Test.sol";
+import {BasicNft} from "../src/BasicNft.sol";
 
 contract BasicNftTest is Test {
     DeployBasicNft public deployer;
     BasicNft public basicNft;
+    address public USER = makeAddr("user");
+    string public constant PUG ="ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
 
     function setUp() public {
         deployer = new DeployBasicNft();
@@ -18,8 +22,19 @@ contract BasicNftTest is Test {
     function testNameIsCorrect() public view {
         string memory expectName = "Dogie";
         string memory actualName = basicNft.name();
-        assert();
+        assert(
+            keccak256(abi.encodePacked(expectName)) ==
+                keccak256(abi.encodePacked(actualName))
+        );
     }
 
+    function testCanMintAndHaveABalance() public { 
+    vm.prank(USER);
+    basicNft.mintNft(PUG);
 
+    assert(basicNft.balanceOf(USER) == 1);
+    assert(keccak256(abi.encodePacked(PUG)) == keccak256(abi.encodePacked(basicNft.tokenURI(0)))
+
+    );
+    }
 }
