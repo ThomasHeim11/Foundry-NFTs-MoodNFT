9189601fda68a36202a5e40100ca52aded387c86
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-23 12:39:14 +0200

    update img✅

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -1,25 +1,5 @@
-// Layout of Contract:
-// version
-// imports
-// errors
-// interfaces, libraries, contracts
-// Type declarations
-// State variables
-// Events
-// Modifiers
-// Functions
-
-// Layout of Functions:
-// constructor
-// receive function (if exists)
-// fallback function (if exists)
-// external
-// public
-// internal
-// private
-// view & pure functions
-
 // SPDX-License-Identifier: MIT
+
 pragma solidity 0.8.19;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
